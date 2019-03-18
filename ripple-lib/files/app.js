a4bc23442cfe0febd4757523375f217ccca286e3
ハトネコエ <ryosuke.yokoe@coincheck.com>
2019-03-18 17:57:50 +0900

    Add a comment

--- ripple-lib/files/app.js
+++ ripple-lib/files/app.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// Example: curl -X POST -H 'Content-Type:application/json' http://127.0.0.1:50080/create_tx -d '{"secret": "shfXBTqLeoqDzkpDpGd9q8Q1uB4ZW", "tx_json": {"TransactionType": "Payment", "Account": "rKXCHMM9U7qeKPeNSb9e7tVtojghjQVmvh", "Destination": "rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe", "Amount": "100", "Fee": "20", "Sequence": "1"}}'
 app.post('/create_tx', function (req, res) {
   const params = req.body;
   const tx = api.sign(JSON.stringify(params['tx_json']), params['secret']);
