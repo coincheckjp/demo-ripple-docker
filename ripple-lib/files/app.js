027b9964ca9f53e4bef21fda6f83b4b7571f90f7
ハトネコエ <ryosuke.yokoe@coincheck.com>
2019-03-18 16:23:33 +0900

    Add GET heartbeat API

--- ripple-lib/files/app.js
+++ ripple-lib/files/app.js
@@ -1,7 +1,7 @@
 'use strict';
 const app = require('express')();
 const bodyParser = require('body-parser');
-const http = require("http").Server(app);
+const http = require('http').Server(app);
 const rippleAPI = require('ripple-lib').RippleAPI;
 
 const api = new rippleAPI({server: 'wss://localhost'});
@@ -12,11 +12,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.post("/create_tx", function (req, res) {
+app.post('/create_tx', function (req, res) {
   const params = req.body;
-  const tx = api.sign(JSON.stringify(params["tx_json"]), params["secret"]);
+  const tx = api.sign(JSON.stringify(params['tx_json']), params['secret']);
 
   res.send(tx);
+});
+
+app.get('/heartbeat', function (_, res) {
+  res.send('OK\n');
 });
 
 http.listen(port);
